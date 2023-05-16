d74d4f9ac40379bb9d32833309aaa1ff9eb86a94
Tito <Tito@DESKTOP-868MOLJ>
2023-05-16 09:34:42 -0400

    ajuste config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://imagina.github.com',
+  baseUrl: '/project-net-framework-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'imagina', // Usually your GitHub org/user name.
+  projectName: 'project-net-framework-docs', // Usually your repo name.
+  
+  deploymentBranch:'gh-pages',
+  trailingSlash:false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
